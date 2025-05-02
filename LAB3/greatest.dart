eccc635ba8f524116cc8bb9547e97895a240f247
tamtamuday <venkateshtamtam7@gmail.com>
2025-05-02 21:34:26 +0530

    Update greatest.dart

--- LAB3/greatest.dart
+++ LAB3/greatest.dart
@@ -1,1 +1,19 @@
 
+import 'dart:io';
+
+void main() {
+    print("Enter first number: ");
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number: ");
+    int num2 = int.parse(stdin.readLineSync()!);
+    print("Enter Third number: ");
+    int num3 = int.parse(stdin.readLineSync()!);
+
+    if(num1 > num2 && num1 > num3) {
+        stdout.write("$num1 is greater than $num2 and $num3");
+    } else if(num2 > num1 && num2 > num3) {
+        stdout.write("$num2 is greater than $num1 and $num3");
+    } else {
+        stdout.write("$num3 is greater than $num1 and $num2");
+    }
+}
