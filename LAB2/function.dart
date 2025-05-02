6b14d841a678b8ee24569dc9e1c1d5998c911030
tamtamuday <venkateshtamtam7@gmail.com>
2025-05-02 21:19:02 +0530

    Update function.dart

--- LAB2/function.dart
+++ LAB2/function.dart
@@ -1,1 +1,28 @@
+import 'dart:io';
+int sum(int a,int b){
+    return(a + b);
+}
+int sub(int a,int b){
+    return(a - b);
+}
+int div(int a,int b){
+    return(a ~/ b);
+}
+int mul(int a,int b){
+    return(a * b);
+}
+int mod(int a,int b){
+    return(a % b);
+}
 
+void main() {
+    print("Enter a and b values:");
+    int a = int.parse(stdin.readLineSync()!);
+    int b = int.parse(stdin.readLineSync()!);
+    stdout.writeln("The sum is : ${sum(a,b)}");
+    stdout.writeln("The Sub is : ${sub(a,b)}");
+    stdout.writeln("The Div is : ${div(a,b)}");
+    stdout.writeln("The Mul is : ${mul(a,b)}");
+    stdout.writeln("The rem is : ${mod(a,b)}");
+
+}
